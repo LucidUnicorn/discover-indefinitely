94aa29c8aa423738e847484b6144ad8c1382e194
LucidUnicorn <25187197+LucidUnicorn@users.noreply.github.com>
2019-09-14 14:44:15 +0100

    add database intermediary client
    The database client is responsible for core database interactions and separates the database logic from the rest of the application. The database is currently designed only as a basic key=value storage engine.

---
+++ spotifybackup/database.py
@@ -0,0 +1,41 @@
+import sqlite3
+from pathlib import Path
+
+
+class DatabaseClient:
+    def __init__(self):
+        self._db_path = (Path(__file__).parent / 'db.sqlite').resolve()
+        connection = self._connect()
+
+        with connection:
+            connection.execute('CREATE TABLE IF NOT EXISTS configuration (key TEXT, value TEXT)')
+
+        connection.close()
+
+    def _connect(self):
+        return sqlite3.connect(self._db_path)
+
+    def get_value(self, key):
+        connection = self._connect()
+
+        with connection:
+            cursor = connection.cursor()
+            cursor.execute('SELECT value FROM configuration WHERE key = ?', (key,))
+            config_value = cursor.fetchone()
+
+        connection.close()
+        return config_value[0]
+
+    def set_value(self, key, value):
+        connection = self._connect()
+
+        with connection:
+            cursor = connection.cursor()
+            cursor.execute('SELECT value FROM configuration WHERE key = ?', (key,))
+
+            if cursor.fetchone() is None:
+                connection.execute('INSERT INTO configuration (key, value) VALUES (?, ?)', (key, value))
+            else:
+                connection.execute('UPDATE configuration SET value = ? WHERE key = ?', (value, key))
+
+        connection.close()
